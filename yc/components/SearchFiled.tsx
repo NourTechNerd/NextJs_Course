f883f6cec5bf8a8e859b7b7a0a05290188adb931
ITSAIDI <noureddinesaidi111@gmail.com>
2025-02-23 13:57:54 +0000

    c

---
+++ yc/components/SearchFiled.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation"; // Import useRouter
+import { Search, X } from "lucide-react";
+
+export default function SearchField() {
+  const [query, setQuery] = useState("");
+  const router = useRouter(); // Initialize router
+
+  const handleSearch = () => {
+    if (query.trim()) {
+      router.push(`/?query=${encodeURIComponent(query)}`); // Navigate to /?query=your_search
+    }
+  };
+
+  return (
+    <div className="
+    search-form
+    ">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleSearch()} // Search on Enter key
+        className="search-input"
+        placeholder="Search for Startups"
+      />
+      <button onClick={handleSearch}>
+        <Search className="text-white bg-black-200 rounded-full w-9 h-9 p-1" />
+      </button>
+      {query && (
+        <button onClick={() => setQuery("")}>
+          <X className="text-white bg-black-200 rounded-full w-9 h-9 p-1" />
+        </button>
+      )}
+    </div>
+  );
+}
