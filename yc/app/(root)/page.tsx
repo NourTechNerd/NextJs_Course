bb29cc89e8744806d241330c3b8ea510bc263781
ITSAIDI <noureddinesaidi111@gmail.com>
2025-02-23 12:47:31 +0000

    c

--- yc/app/(root)/page.tsx
+++ yc/app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import SearchFiled from '../components/SearchFiled'
+
+
+
 
 export default function Home() {
   return (
     <section className='pink_container'>
       <h1 className='heading'> Pith your Startup and <br />Connect with Enterpreneurs</h1>
       <p className='sub-heading'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      <SearchFiled  />
     </section>
   )
 }
